f5b56766a99568dab5c80c8cf9a5b3695d545c0a
Anthony Dugouchet <adgouchet@lafourchette.com>
2018-02-20 17:29:35 +0100

    add Joi validation schema

--- src/methods/insertPkpass.js
+++ src/methods/insertPkpass.js
@@ -1,15 +1,12 @@
 'use strict';
 const Joi = require('joi');
-
+const { createPkpassSchema } = require('../schemas/pkpassSchema');
 module.exports = {
   name: 'insertPkpass',
   description: 'insert pkpass',
   config: {
     validate: {
-      payload: Joi.object().keys({
-        name: Joi.string(),
-        restaurantName: Joi.string(),
-      })
+      payload: createPkpassSchema
     },
     handler: ({payload}, databaseClient) => {
       console.log('INPUT !!!',payload);
